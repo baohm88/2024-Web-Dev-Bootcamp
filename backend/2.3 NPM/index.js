fdef6fbea357101f1b77343cdba5ea7dee341e51
Bao Ha <baohm88@gmail.com>
2024-06-10 13:35:31 +0700

    Added npm

---
+++ backend/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+
+// import superheroes from 'superheroes';
+// const name = superheroes.random()
+// console.log(`I'm + ${name}!`);
+
+
+import {randomSuperhero} from 'superheroes';
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
